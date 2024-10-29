60cc58d14daf10417c72e071b3835d006bcd4a57
Prashant-kumar-321 <pk6176838@gmail.com>
2024-10-29 14:16:05 +0530

    Finish implementing header and hero section

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -7,13 +7,25 @@ module.exports = {
         "primary": "#28bf96", 
         "light-primary": "#209677",
         "secondary": "#defcf4",
-        "light-gray": "#7e7684"
+        "light-gray": "#7e7684",
+        "extralight-primary": "#43ff6433",
       },
       fontFamily:{
         "poppins": ['Poppins', 'sans-serif'],
+      }, 
+      translate:{
+        'topleft': 'translate(calc(-50% - 12rem)), calc(-50% - 8rem)',
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function({addUtilities}){
+      addUtilities({
+        '.translate-topleft':{
+          transform: 'translate(calc(-50% - 12rem), calc(-50% - 8rem))',
+        }
+      }); 
+    }
+  ],
 }
 
