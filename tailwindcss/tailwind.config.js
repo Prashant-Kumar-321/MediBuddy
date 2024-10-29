3b586e5fae5e473bc610ee37189a8c41ecce6473
Prashant-kumar-321 <pk6176838@gmail.com>
2024-10-29 09:22:06 +0530

    Finish implementing Header section

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['../index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "primary": "#28bf96", 
+        "light-primary": "#209677",
+        "secondary": "#defcf4",
+        "light-gray": "#7e7684"
+      },
+      fontFamily:{
+        "poppins": ['Poppins', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
